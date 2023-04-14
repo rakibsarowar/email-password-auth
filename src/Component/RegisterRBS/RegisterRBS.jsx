4430df07b52e21fa35ea4eae463da6c4397a834b
rakibsarowar <rakibsarowar@gmail.com>
2023-04-14 22:37:24 +0600

    added on submit event handler

--- src/Component/RegisterRBS/RegisterRBS.jsx
+++ src/Component/RegisterRBS/RegisterRBS.jsx
@@ -3,13 +3,24 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const RegisterRBS = () => {
+    const handleRegister =(event)=>{
+        event.preventDefault()
+        const email=event.target.email.value; 
+        const password=event.target.password.value; 
+
+        console.log(email, password)
+    }
+
+
     return (
+
+
         <div className='w-50 mx-auto'>
             <h1>Please Register</h1>
-            <Form>
+            <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control name='email' type="email" placeholder="Enter email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -17,7 +28,7 @@ const RegisterRBS = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control name='password' type="password" placeholder="Password" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
