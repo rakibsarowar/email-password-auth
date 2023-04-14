553a933ff1a0249e5641b64611e301056e617e8a
rakibsarowar <rakibsarowar@gmail.com>
2023-04-14 22:02:55 +0600

    added event target value

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Register = () => {
+    const handleEmailChange = (event) =>{
+console.log(event.target.value)
+    }
     return (
         <div>
                 <h2>Please Register</h2>
                 <form>
-                    <input type="email" name="email" id="email" placeholder='your email' /> <br />
+                    <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='your email' /> <br />
 
                     <input type="password" name="password" id="" placeholder='your password' /> <br />
                     <input type="submit" value="Register" />
