f0c564866781dff1ba1c8c24230762816f480bd3
rakibsarowar <rakibsarowar@gmail.com>
2023-04-14 22:13:00 +0600

    added onsubmit

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -2,26 +2,32 @@ import React, { useState } from 'react';
 
 const Register = () => {
     const [email, setEmail] = useState('');
-    const [emailBlar, setEmailBlar] =useState('')
+    const [emailBlar, setEmailBlar] = useState('')
 
-    const handleEmailChange = (event) =>{
-console.log(event.target.value)
+    // input onchange function --------------------
+    const handleEmailChange = (event) => {
+        console.log(event.target.value)
         setEmail(event.target.value)
     }
 
-    const handlePasswordBlar = (event) =>{
+    // input blar function --------------------
+    const handlePasswordBlar = (event) => {
         console.log(event.target.value);
         setEmailBlar(event.target.value)
     }
+
+    const handleSubmit = (event) => {
+        console.log(event.target)
+    }
     return (
         <div>
-                <h2>Please Register</h2>
-                <form>
-                    <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='your email' /> <br />
+            <h2>Please Register</h2>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='your email' /> <br />
 
-                    <input onBlur={handlePasswordBlar}  type="password" name="password" id="" placeholder='your password' /> <br />
-                    <input type="submit" value="Register" />
-                </form>
+                <input onBlur={handlePasswordBlar} type="password" name="password" id="" placeholder='your password' /> <br />
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
