7f18d372d983f084023f2a2001d1c922bc619109
rakibsarowar <rakibsarowar@gmail.com>
2023-04-14 21:57:58 +0600

    added a form

--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-                <h2>This is Register</h2>
+                <h2>Please Register</h2>
+                <form>
+                    <input type="email" name="email" id="email" placeholder='your email' /> <br />
+
+                    <input type="password" name="password" id="" placeholder='your password' /> <br />
+                    <input type="submit" value="Register" />
+                </form>
         </div>
     );
 };
